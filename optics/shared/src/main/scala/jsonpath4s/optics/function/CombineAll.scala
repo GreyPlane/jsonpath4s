ef31677896a52ab30da62a6e377b3b206c8491eb
GreyPlane <709327148@qq.com>
2026-02-11 18:17:42 +0800

    use new context list syntax

--- optics/shared/src/main/scala/jsonpath4s/optics/function/CombineAll.scala
+++ optics/shared/src/main/scala/jsonpath4s/optics/function/CombineAll.scala
@@ -4,7 +4,7 @@ import cats.implicits.given
 import cats.{Applicative, Monoid, Traverse}
 import monocle.{Fold, Traversal}
 
-def combineAll[A, T[_]: Traverse: Applicative](traversals: T[Traversal[A, A]]): Fold[A, A] = new Fold[A, A]:
+def combineAll[A, T[_]: {Traverse, Applicative}](traversals: T[Traversal[A, A]]): Fold[A, A] = new Fold[A, A]:
   def foldMap[M: Monoid](f: A => M)(s: A): M = {
     Applicative[T].map(traversals)(_.foldMap(f)(s)).foldLeft(Monoid[M].empty)(_ combine _)
   }
